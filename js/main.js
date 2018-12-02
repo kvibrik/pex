4ae5874054c86a50c1ba010509bf9aae4b1dd2d8
kvibrik <kvibr@mail.ru>
2018-12-02 20:41:12 +0500

    адаптив начал

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
-
+$(function(){$(window).width()<="950"&&($(".header__hamb").on("click",function(){$(".header__menu").show(500).css("display","flex")}),$(".header__menu-close").on("click",function(){$(".header__menu").hide(500)}))});
 //# sourceMappingURL=main.js.map
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+  if ($(window).width() <= '950') {
+    $(".header__hamb").on("click", function () {
+      $(".header__menu").show(500).css("display", "flex");
+    });
+
+    $(".header__menu-close").on("click", function () {
+      $(".header__menu").hide(500);
+    });
+  }
+});
